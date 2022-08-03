4310350ca2e9f78ce4bdaaf16b04be5deeb875e2
seka18 <109748964+seka18@users.noreply.github.com>
2022-08-03 16:07:33 +0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,39 +1,41 @@
 #include <iostream>
-
 using namespace std;
-
-class Queue {
+class Queue 
+   {
     public:
-        Queue(int size = 10); // constructor
+        Queue(int size = 40); // constructor
         ~Queue() { delete [] values; } // destructor
         bool IsEmpty(void);
         bool IsFull(void);
-        bool Enqueue(double x);
-        bool Dequeue(double & x);
+        bool Enqueue(char x);
+        char Dequeue();
         void DisplayQueue(void);
     private:
         int front; // front index
         int rear; // rear index
         int counter; // number of elements
         int maxSize; // size of array queue
-        double* values; // element array
-    };
-    Queue::Queue(int size /* = 10 */) {
-    values = new double[size];
-    maxSize = size;
-    front = 0;
-    rear = -1;
-    counter = 0;
+        char *values; // element array
+   };
+    Queue::Queue(int size /* = 10 */) 
+    {
+        values = new char[size];
+        maxSize = size;
+        front = 0;
+        rear = -1;
+        counter = 0;
     }
-    bool Queue::IsEmpty() {
-    if (counter) return false;
+    bool Queue::IsEmpty() 
+    {
+        if (counter) return false;
+        else return true;
+    }
+    bool Queue::IsFull() 
+    {
+    if (counter < maxSize) return false;
     else return true;
     }
-    bool Queue::IsFull() {
-if (counter < maxSize) return false;
-else return true;
-}
-bool Queue::Enqueue(double x) {
+bool Queue::Enqueue(char x) {
 if (IsFull()) {
 cout << "Error: the queue is full." << endl;
 return false;
@@ -48,46 +50,20 @@ else {
     return true;
     }
 }
-bool Queue::Dequeue(double & x) {
+char Queue::Dequeue() {
     if (IsEmpty()) {
     cout << "Error: the queue is empty." << endl;
     return false;
     }
 else {
     // retrieve the front item
-    x = values[front];
+    char x = values[front];
     // move front
     front = (front + 1) % maxSize;
     // update counter
     counter--;
-return true;
+		return x;
+	}
 }
-}void Queue::DisplayQueue() {
-    cout << "front -->";
-    for (int i = 0; i < counter; i++) {
-    if (i == 0) cout << "\t";
-    else cout << "\t\t";
-    cout << values[(front + i) % maxSize];
-    if (i != counter - 1)
-    cout << endl;
-    else
-    cout << "\t<-- rear" << endl;
 
-}
-}
-int main(void) {
-    Queue queue(5);
-    cout << "Enqueue 5 items." << endl;
-    for (int x = 0; x < 5; x++)
-queue.Enqueue(x);
-    cout << "Now attempting to enqueue again..." << endl;
-queue.Enqueue(5);
-queue.DisplayQueue();
-    double value;
-queue.Dequeue(value);
-    cout << "Retrieved element = " << value << endl;
-queue.DisplayQueue();
-queue.Enqueue(7);
-queue.DisplayQueue();
-return 0;
-}
+   
